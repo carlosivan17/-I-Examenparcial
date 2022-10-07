650dcedaa9a016ebd66b69a3310e07c009300ad9
CarlosDelgado <carlosdelgado1711@gmail.com>
2022-10-07 17:57:48 -0600

    Se subio IExamenparcial

---
+++ src/CalculoDepreciacionActivos/ELhogar.java
@@ -0,0 +1,61 @@
+package CalculoDepreciacionActivos;
+
+import java.util.Scanner;
+
+public class ELhogar {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		//Datos de Entrada
+		int camioneta;
+		int vidautil;
+		int valorresidual;
+		double depreciacionanual = 0;
+		
+		System.out.println("Ingrese el costo de la camioneta: ");
+		camioneta  = sc.nextInt();
+		System.out.println("Ingrese la vida util de la camioneta: ");
+		vidautil  = sc.nextInt();
+		System.out.println("Ingrese el valor residual: ");
+		valorresidual  = sc.nextInt();
+		
+		//Procesamiento
+		depreciacionanual = (camioneta - valorresidual)/vidautil;
+		
+		//Datos de salida
+		while(vidautil > 0) {
+			System.out.println("Anios = " + vidautil);
+			vidautil--;
+		}
+		System.out.println("La depreciacion anual es de: " + depreciacionanual);
+		
+		
+		
+		
+
+	}
+	public static void Metododeunidadesproducidas(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		//Datos de entrada
+		int costedelactivo;
+		int valordesecho;
+		int vidautil;
+		double vidautilenunidades = 0;
+		
+		System.out.println("Ingrese el coste del activo: ");
+		costedelactivo = sc.nextInt();
+		System.out.println("Ingrese el valor de desecho: ");
+		valordesecho = sc.nextInt();
+		System.out.println("Ingrese la vida util: ");
+		vidautil = sc.nextInt();
+		
+		for(vidautilenunidades = 0; vidautilenunidades == vidautil; vidautilenunidades++) {
+			System.out.println("");
+		}
+		
+		
+		
+		
+	}
+
+}
